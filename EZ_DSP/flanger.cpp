8e3d40ffdabfb0588f2ac499bd49483d21e90607
DavidJones10 <102177843+DavidJones10@users.noreply.github.com>
2023-09-06 15:00:53 -0400

    Finished flanger class

--- EZ_DSP/flanger.cpp
+++ EZ_DSP/flanger.cpp
@@ -6,11 +6,18 @@ using namespace EZ_DSP;
 void Flanger::Init(float sample_rate)
 {
     sampleRate = sample_rate;
+    LfoPhase = 0.f;
 }
 
 float Flanger::Process(float in)
 {
-
+    float lfoSignal;
+    if (modType == 0) lfoSignal = processLfoSin();
+    else lfoSignal = processLfoTri();
+    delLine.setDelay(1.f + lfoSignal + delay);
+    float out = delLine.read();
+    delLine.write(in + out * feedback);
+    return in *(1-dryWet) + out*dryWet; 
 }
 
 void Flanger::setFeedback(float fback)
@@ -29,26 +36,50 @@ void Flanger::setDelayMs(float delayMs)
 {
     delayMs = fmax(.1, delayMs);
     delay = delayMs * .001f * sampleRate;
-
+    LfoAmp = fmin(LfoAmp, delay);
 }
 
 void Flanger::setLfoDepth(float depth)
 {
     depth = fclamp(depth, 0, .95f);
-     
+    LfoAmp = depth * delay;
 }
 
 void Flanger::setLfoFrequency(float freq)
 {
+    if (modType==0) LfoFreq = freq/sampleRate;
+    else
+    {
+        freq = freq * 4.f / sampleRate;
+        freq *= LfoFreq < 0.f ? -1.f : 1.f; 
+        LfoFreq = fclamp(freq,-.25f, .25f);
+    }
+}
 
+void Flanger::setDryWet(float wet)
+{
+    dryWet = wet;
 }
 
 float Flanger::processLfoSin()
 {
-
+    LfoPhase += LfoFreq;
+    if (LfoPhase >= 1.f) LfoPhase -= 1;
+    return LfoAmp * sin(TWOPI_F * LfoPhase);
 }
 
 float Flanger::processLfoTri()
 {
-
+    LfoPhase += LfoFreq;
+    if (LfoPhase > 1.f)
+    {
+        LfoPhase = 1.f -(LfoPhase -1.f);
+        LfoPhase *= -1.f;
+    }
+    else if (LfoPhase < -1.f)
+    {
+        LfoPhase = -1.f - (LfoPhase + 1.f); 
+        LfoPhase *= -1.f;
+    }
+    return LfoPhase * LfoAmp;
 }
