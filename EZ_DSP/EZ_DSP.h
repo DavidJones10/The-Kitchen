d6f66f9d59254117d1ff687b9e42c2e57e518320
DavidJones10 <102177843+DavidJones10@users.noreply.github.com>
2023-09-11 22:25:58 -0400

    Finished Chorus class, added EZ_DSP.h for includes, updated some stuff in the delay_line class, deleted sequencer.cpp

---
+++ EZ_DSP/EZ_DSP.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef EZ_DSP_H
+#define EZ_DSP_H
+
+#include "dsp.h"
+#include "delay_line.h"
+#include "chorus.h"
+#include "flanger.h"
+
+
+#endif
--- EZ_DSP/chorus.h
+++ EZ_DSP/chorus.h
@@ -12,47 +12,48 @@ class ChorusEngine
         ChorusEngine(){}
         ~ChorusEngine(){}
         /** Initializes the chorus engine
-         * @param sample_rate 
+         * @param sample_rate sample rate for internal calculations
          */
         void init(float sample_rate);
         /** Processes chorus engine
             \param input float input from audio engine
         */
         float process(float input);
+        
         /** Sets feedback for internal delay 
-            \param fback expects 0-1 float value
+            \param fback expects 0-1 
         */
         void setFeedback(float fback);
 
         /** Sets amplitude of modulator
-            \param depth expects 0-1 float value
+            \param depth expects 0-1 
         */
         void setLfoDepth(float depth);
 
         /** Sets modulation rate
-            \param frequency Hz value between -20 and 20 Hz
+            \param frequency expects 0-10 Hz
         */
         void setLfoFrequency(float frequency);
 
-        /** Sets internal delay as a float range
-            \param delayScaled expects value from 0-1 Scales to .1-7ms
+        /** Sets internal delay using float range
+            \param delayScaled expects 0-1 Scales to 10-35ms
         */
         void setDelay(float delayScaled);
 
         /** Sets internal delay in ms
-            \param delayMS expects value in ms between .1 and 7ms
+            \param delayMS expects 10-35ms
         */
         void setDelayMs(float delayMS);
 
-        /** Modulator wave type mode selector. 0: SIN; 1: TRI
-            \param mode Defaults to 0, outOfRange = 0
-        */
-        void setModMode(int mode);
-
         /** Sets dry wet for chorus engine
-            \param wet expects value from 0-1
+            \param wet expects 0-1
         */
         void setDryWet(float wet);
+        
+        /** Sets offset for chorus, only useful if you have 2 chorusEngines
+            \param offset offset for chorus modulator, expects 0-1
+         */
+        void addPhaseOffset(float offset);
     private:
         float feedback;
         float LfoFreq;
@@ -64,14 +65,7 @@ class ChorusEngine
         int8_t motType = 0;
         static constexpr int32_t maxDelaySize = 2400; // 50ms max
         DelayLine<float, maxDelaySize> delLine;
-        enum ModModes
-        {
-            SIN = 0,
-            TRI = 1
-        };
-        
-        float processLfoSin();
-        float processLfoTri();
+        float processLfo();
 };
 
 class Chorus
@@ -79,93 +73,108 @@ class Chorus
     public:
         Chorus(){}
         ~Chorus(){}
-        /** 
-            \param sample_rate 
+        /** Initializes chorus(es)
+            \param sample_rate sample rate for internal calculations
         */
         void init(float sample_rate);
 
-        /** 
-            \param input
+        /** Processes input and returns effected output 
+         *  Returns left channel only 
+            \param input sample to be processed and effected
         */
         float process(float input);
 
-        /**  */
+        // Returns value of left channel of effected signal
+        // Must be called after process()
         float getLeft();
 
-        /**  */
+        // Returns value of right channel of effected signal
+        // Must be called after process()
         float getRight();
 
-        /** 
-            \param panLeft
-            \param panRight
+        /** Sets pan for each channel individually
+            \param panLeft expects 0-1, 0 is left, 1 is right
+            \param panRight expects 0-1, 0 is left, 1 is right
         */
         void setPan(float panLeft, float panRight);
 
-        /** 
-            \param pan 
+        /** Sets pan for both channels 
+            \param pan expects 0-1, 0 is left, 1 is right
         */
         void setPan(float pan);
 
-        /** 
-            \param depthLeft 
-            \param depthRight
+        /** Sets lfo depth for each channel independently
+            \param depthLeft expects 0-1
+            \param depthRight expects 0-1
         */
         void setLfoDepth(float depthLeft, float depthRight);
 
-        /** 
-            \param depth 
+        /** Sets lfo depth for both channels
+            \param depth  expects 0-1
         */
         void setLfoDepth(float depth);
 
-        /** 
-            \param freqLeft 
-            \param freqRight 
+        /** Sets lfo frequency for each channel independently
+            \param freqLeft expects 0-10Hz
+            \param freqRight expects 0-10Hz
         */
         void setLfoFreq(float freqLeft, float freqRight);
 
-        /** 
-            \param freq 
+        /** Sets lfo frequency for both channels
+            \param freq expdects 0-10Hz
         */
         void setLfoFreq(float freq);
 
-        /** 
+        /** Sets internal delay for each channel individually 
+         *    using float range
             \param delayLeft 
             \param delayRight
         */
         void setDelay(float delayLeft, float delayRight);
 
-        /** 
-            \param delay 
+        /** Sets internal delay for both channels using float range
+            \param delay expects 0-1
         */
         void setDelay(float delay);
 
-        /** 
-            \param msLeft 
-            \param msRight 
+        /** Sets internal delay in ms for both channels
+            \param msLeft expects 10-35ms
+            \param msRight expects 10-35ms
         */
         void setDelayMs(float msLeft, float msRight);
 
-        /** 
-            \param ms 
+        /** Sets internal delay in ms for both channels 
+            \param ms expects 10-35ms
         */
         void setDelayMs(float ms);
 
-        /** 
-            \param fbackLeft
-            \param fbackRight
+        /** Sets feedback for each channelsindependently
+            \param fbackLeft expects 0-1
+            \param fbackRight expects 0-1
         */
         void setFeedback(float fbackLeft, float fbackRight);
 
-        /** 
-            \param fback 
+        /** Sets feedback for both channels 
+            \param fback  expects 0-1
         */
         void setFeedback(float fback);
+
+        /** Sets offset between Left and Right channel for stereo chorus
+            \param offset expects 0-1
+         */
+        void setPhaseOffset(float offset);
+        
+        /** Sets dry/wet for both channels
+            \param wet expects 0-1
+         */
+        void setDryWet(float wet);
 
     private:
         ChorusEngine engines[2];
         float gainFraction;
         float pans[2];
         float signalL, signalR;
+        float phaseOffset;
 };
 
 }
