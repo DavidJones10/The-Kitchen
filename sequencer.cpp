a4cf78e0e50427cee2129c17f0d6b5b2e483350c
ryansauce <144172416+ryansauce@users.noreply.github.com>
2023-09-19 16:50:55 -0400

    update sequencer

---
+++ sequencer.cpp
@@ -0,0 +1,24 @@
+int dur = 3;
+int fs = 44100;
+int totalSamples = dur * fs;
+int numSteps = 8;
+int stepLength = totalSamples / numSteps;
+
+bool kick[8];
+bool snare[8];
+bool hat[8];
+bool crash[8];
+
+bool trigSound( int totalSamples, int numSteps, int stepLength, bool kick, bool snare, bool hat, bool crash)
+{
+    float sequence[totalSamples];
+    for(int i = 0; i < numSteps; i++)
+    {
+        sequence[]
+    }
+}
+
+int updateSeq()
+{
+
+}
