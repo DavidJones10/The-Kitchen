171464b1a0e899e73582bb824978826b25d7b134
DavidJones10 <102177843+DavidJones10@users.noreply.github.com>
2023-09-26 16:19:37 -0400

    reformatted kickDrum file to same as rest

--- EZ_DSP/kickDrum.cpp
+++ EZ_DSP/kickDrum.cpp
@@ -1,10 +1,11 @@
 #include "kickDrum.h"
-#include "oscillator.cpp"
+#include "JOEoscillator.cpp"
 
-kickDrum::kickDrum(double sampleRate,int waveType) {
+kickDrum::kickDrum(float sampleRate,int waveType) 
+{
     this->sampleRate = sampleRate;
-    waveformSize = 44100; 
-    waveform = new double[waveformSize];
+    waveformSize = 48000; 
+    waveform = new float[waveformSize];
 
     volumeDecay = 0.0;
     volumeSustain = 0.0;
@@ -13,27 +14,31 @@ kickDrum::kickDrum(double sampleRate,int waveType) {
     pitchEnd = 0.0;
     lfoFrequency = 1.0; 
     lfoPhase = 0.0;
-
 }
 
 
-kickDrum::~kickDrum() {
+kickDrum::~kickDrum() 
+{
     delete[] waveform;
 }
 
-void kickDrum::setVolumeEnvelope(double decay, double sustain) {
+void kickDrum::setVolumeEnvelope(float decay, float sustain) 
+{
     volumeDecay = decay;
     volumeSustain = sustain;
 }
 
-void kickDrum::setPitchEnvelope(double decay, double pitchStart, double pitchEnd) {
+void kickDrum::setPitchEnvelope(float decay, float pitchStart, float pitchEnd) 
+{
     pitchDecay = decay;
     this->pitchStart = pitchStart;
     this->pitchEnd = pitchEnd;
 }
 
-void kickDrum::selectWaveform(int waveType) {
-   switch (waveType) {
+void kickDrum::selectWaveform(int waveType) 
+{
+   switch (waveType) 
+   {
         case 0: // Sine wave
             generateSineWave(this->waveform, waveformSize);
             break;
@@ -46,43 +51,49 @@ void kickDrum::selectWaveform(int waveType) {
     }
 }
 
-double kickDrum::calculateEnvelope(double time, double decay, double sustain) {
-    if (time < 0.0) {
+float kickDrum::calculateEnvelope(float time, float decay, float sustain) 
+{
+    if (time < 0.0) 
+    {
         return 0.0;  // Envelope not started yet
-    } else if (time < decay) {
+    } 
+    else if (time < decay) {
         return 1.0 - (1.0 - sustain) * (1.0 - exp(-time / decay));
-    } else {
+    } 
+    else 
+    {
         return sustain;
     }
 }
 
-void kickDrum::generateAudio(double* buffer, int numSamples) {
-    for (int i = 0; i < numSamples; ++i) {
+float kickDrum::generateAudio(float in, int index) 
+{
         // Calculate the volume envelope value at this time
-        double volumeEnvelope = calculateEnvelope(i / sampleRate, volumeDecay, volumeSustain);
+        float volumeEnvelope = calculateEnvelope(index / sampleRate, volumeDecay, volumeSustain);
 
         // Calculate the pitch envelope value at this time
-        double pitchEnvelope = calculateEnvelope(i / sampleRate, pitchDecay, 1.0);  // Sustain level of 1.0
+        float pitchEnvelope = calculateEnvelope(index / sampleRate, pitchDecay, 1.0);  // Sustain level of 1.0
 
         // Calculate the current pitch based on the pitch envelope
-        double currentPitch = pitchStart + (pitchEnd - pitchStart) * pitchEnvelope;
+        float currentPitch = pitchStart + (pitchEnd - pitchStart) * pitchEnvelope;
 
         // Calculate the LFO value (e.g., using a sine wave)
-        double lfoValue = sin(2.0 * M_PI * lfoFrequency * i / sampleRate);
+        float lfoValue = sin(2.0 * M_PI * lfoFrequency * index / sampleRate);
 
         // Adjust modulationAmount to control the depth of modulation
-        double modulatedPitch = currentPitch + modulationAmount * lfoValue;
+        float modulatedPitch = currentPitch + modulationAmount * lfoValue;
 
         // Calculate the angular frequency for the oscillator based on modulatedPitch
-        double angularFrequency = 2.0 * M_PI * modulatedPitch;
+        float angularFrequency = 2.0 * M_PI * modulatedPitch;
 
         // Initialize the phase of the oscillator
-        double phase = 0.0;
+        float phase = 0.0;
 
         // Calculate the current sample value by modulating the waveform with volume and pitch
-        double sampleValue = 0.0;  // Initialize the sample value
+        float sampleValue = 0.0;  // Initialize the sample value
 
-        for (int j = 0; j < numOscillatorCycles; ++j) {
+        for (int j = 0; j < numOscillatorCycles; ++j) 
+        {
             // Generate the sample by summing oscillators with different phases
             sampleValue += sin(phase);
             
@@ -97,8 +108,8 @@ void kickDrum::generateAudio(double* buffer, int numSamples) {
         sampleValue *= volumeEnvelope;
 
         // Store the sample in the buffer
-        buffer[i] = sampleValue;
-    }
+        return sampleValue;
+    
 }
 
 
--- EZ_DSP/kickDrum.h
+++ EZ_DSP/kickDrum.h
@@ -1,32 +1,33 @@
 #pragma once
 
-class kickDrum {
+class kickDrum 
+{
 public:
-    kickDrum(double sampleRate, int waveType);
+    kickDrum(float sampleRate, int waveType);
     ~kickDrum();
 
-    void setVolumeEnvelope(double decay, double sustain);
-    void setPitchEnvelope(double decay, double pitchStart, double pitchEnd);
+    void setVolumeEnvelope(float decay, float sustain);
+    void setPitchEnvelope(float decay, float pitchStart, float pitchEnd);
     void selectWaveform(int waveform);
-    void setModulationAmount(double amount);
+    void setModulationAmount(float amount);
     void setNumOscillatorCycles(int cycles);
 
-    void generateAudio(double* buffer, int numSamples);
+    float generateAudio(float in, int index);
 
 private:
     int waveType;
-    double sampleRate;
-    double* waveform;
+    float sampleRate;
+    float* waveform;
     int waveformSize;
-    double volumeDecay;
-    double volumeSustain;
-    double pitchDecay;
-    double pitchStart;
-    double pitchEnd;
-    double lfoFrequency;
-    double lfoPhase;
-    double modulationAmount;
+    float volumeDecay;
+    float volumeSustain;
+    float pitchDecay;
+    float pitchStart;
+    float pitchEnd;
+    float lfoFrequency;
+    float lfoPhase;
+    float modulationAmount;
     int numOscillatorCycles;
 
-    double calculateEnvelope(double time, double decay, double sustain);
+    float calculateEnvelope(float time, float decay, float sustain);
 };
