092838c81f6b2ac05d04acfffbf88a571024618f
DavidJones10 <102177843+DavidJones10@users.noreply.github.com>
2023-10-03 03:05:06 -0400

    Finished oscillator class

--- EZ_DSP/oscillator.cpp
+++ EZ_DSP/oscillator.cpp
@@ -7,12 +7,48 @@ void Oscillator::init(float sample_rate)
     sampleRate =  sample_rate;
     frequency      = 100.0f;
     amplitude       = 0.5f;
-    pulsWidth        = 0.5f;
+    pulseWidth        = 0.5f;
     phase             = 0.0f;
     phaseInc =          getPhaseInc(frequency);
     waveType  = SIN;
-    endOfCycle             = true;
-    endOfRise             = true;
+    endOfCycle_             = true;
+    endOfRise_             = true;
+}
 
+float Oscillator::process()
+{
+    float output, t;
+    switch (waveType)
+    {
+    case SIN:
+        output = sinf(phase * TWOPI_F);
+        break;
+    case TRI:
+        t = -1.0f + (2.0f * phase);
+        output = 2.0f * (fabsf(t) - 0.5f);
+        break;
+    case SAW:
+        output = -1.0f * ((phase * 2.0f) - 1.0f);
+        break;
+    case SQUARE:
+        output = phase < pulseWidth ? (1.0f) : -1.0f;
+        break;
+    default:
+        output = 0.f;
+        break;
+    }
+
+    phase += phaseInc;
+
+    if (phase > 1.0f)
+    {
+        phase -= 1.f;
+        endOfCycle_ = true;
+    }
+    else endOfCycle_ = false;
+
+    endOfRise_ = (phase - phaseInc < .5f && phase >= .5f);
+    
+    return output * amplitude;
 }
 
--- EZ_DSP/oscillator.h
+++ EZ_DSP/oscillator.h
@@ -13,25 +13,51 @@ public:
     {
         SIN, TRI, SAW, SQUARE, LAST
     };
-   
+    /** Initializes the oscillator
+     * @param sample_rate sample rate for audio playback
+     */
     void init(float sample_rate);
-
+    // Processes the oscillator; should be called on a sample by sample basis
     float process();
-
-    void setAmplitude(float amp) {amplitude = amp;}
-
-    void setFreq(float freq) {frequency = freq;}
-
-    void setWaveType(u_int8_t type) {waveType = type < LAST ? type : SIN;}
+    /** Sets amplitude for the oscillator
+     * @param amp amplitude for the oscillator
+     */
+    inline void setAmplitude(float amp) {amplitude = amp;}
+    /** Sets frequency of the oscillator
+     * @param freq frequency in Hz
+     */
+    inline void setFreq(float freq) {frequency = freq;}
+    /** Sets the wave type for the oscillator
+     * @param type wave type- 1:SIN, 2:TRI, 3:SAW, 4:SQUARE
+     */
+    inline void setWaveType(u_int8_t type) {waveType = type < LAST ? type : SIN;}
+    /** Sets pulse width for square waveform 
+     * @param pw expects 0-1
+     */
+    inline void setPulseWidth(float pw) {pulseWidth = fclamp(pw, 0.f, 1.f);}
+    // bool that returns true if the oscillator phase is rising
+    inline bool isRising() {return phase < .5f;}
+    // bool that returns true if the oscillator phase is falling
+    inline bool isFalling() {return phase >= .5f;}
+    /** Adds given phase to current internal phase
+     * @param phase_ phase to add to current value
+     */
+    inline void addPhase(float phase_) {phase += phase_;}
+    /** Resets phase to given value
+     * @param phase_ phase to reset to, defaults to 0
+     */
+    inline void resetPhase(float phase_ = 0) {phase = phase_;}
+    // returns true when the phase stops rising
+    inline bool endOfRise() {return endOfRise_;}
+    // returns true when a phase cycle is completed
+    inline bool endOfCycle() {return endOfCycle_;}
     
 private:
-    float amplitude, frequency, pulsWidth, sampleRate, phase, phaseInc;
-    bool endOfRise, endOfCycle;
+    float amplitude, frequency, pulseWidth, sampleRate, phase, phaseInc;
+    bool endOfRise_, endOfCycle_;
     u_int8_t waveType;
 
     float getPhaseInc(float freq) {return freq/sampleRate;};
-
-
 
 };
 
